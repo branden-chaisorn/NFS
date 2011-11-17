b073351602d4f0f242c61e36484df761d3c9806d
Andrew Downing <Downing22@utexas.edu>
2011-11-16 21:12:40 -0600

    Branden 1st changes.

--- SNFS.cc
+++ SNFS.cc
@@ -5,49 +5,57 @@
 
 #include "fs_client.h"
 
+#include "fs_crypt.h"
+
+#include "fs_param.h"
+
+#include "fs_server.h"
+
 using namespace std;
 
 
 
-fs_session(string user, string password, , ){
-
-}
 
 
 
-
-
-
-
- 
-
-main(int argc, char *argv[])
-
+void main(int argc, char *argv[])
 {
 
   char *server;
-
   int server_port;
-
   int session, seq=0;
-
   char buf[10];
+  FILE *stdin;
+  string usernames[];
+  string passwords[];
+  string first_usrn="";
+  string first_pwrd="";
 
- 
+  int index = 0;
+
 
   if (argc != 3)
-
     {
-
       cout << "error: usage: " << argv[0] << "\n"; exit(1);
-
     }
 
- 
 
   server = argv[1];
+  server_port = atoi(argv[2]); //returns the integer version
 
-  server_port = atoi(argv[2]);
+  //open the password file
+  UANDP = fopen(argv[3], "r");
+
+  //do a reading of the usernames and passwords
+  while(!feof(stdin)){
+   scanf(%s, );
+   scanf(%s, );
+
+   index++;
+  }
+
+  //close the file
+  fclose(stdin);
 
   // fs_session("user1", "password1", server, server_port, &session);
 
