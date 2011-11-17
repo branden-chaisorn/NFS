21c8cea9702c811e831bff95bcd8bacf3e419e9f
Branden <branden.chaisorn@gmail.com>
2011-11-16 19:35:36 -0600

    same SNFS but .cc

---
+++ SNFS.cc
@@ -0,0 +1,62 @@
+
+#include <iostream>
+
+#include <cstdlib>
+
+#include "fs_client.h"
+
+using namespace std;
+
+
+
+fs_session(){
+
+}
+
+
+
+
+
+
+
+ 
+
+main(int argc, char *argv[])
+
+{
+
+  char *server;
+
+  int server_port;
+
+  int session, seq=0;
+
+  char buf[10];
+
+ 
+
+  if (argc != 3)
+
+    {
+
+      cout << "error: usage: " << argv[0] << "\n"; exit(1);
+
+    }
+
+ 
+
+  server = argv[1];
+
+  server_port = atoi(argv[2]);
+
+  // fs_session("user1", "password1", server, server_port, &session);
+
+  //fs_create("user1", "password1", session, seq++, "tmp");
+
+  //fs_append("user1", "password1", session, seq++, "tmp", "abc", 3);
+
+  //fs_read("user1", "password1", session, seq++, "tmp", 1, buf, 2);
+
+  //fs_delete("user1", "password1", session, seq++, "tmp");
+
+}
