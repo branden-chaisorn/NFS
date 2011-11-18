79ef5e9d0e6e3404adaa55e116bbcc9aee8ea571
Andrew Downing <Downing22@utexas.edu>
2011-11-17 21:24:44 -0600

    fs.cc which now prints up socket port and user names and passwords created on: 11/17

--- fs.cc
+++ fs.cc
@@ -1,12 +1,13 @@
-#include <iostream>
 #include <cstdlib>
 #include <stdio.h>
+#include <stdlib.h>
 #include "fs_client.h"
 #include "fs_server.h"
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <fstream>
 
 
 using namespace std;
@@ -22,23 +23,14 @@ int main(int argc, char *argv[])
     int UorP = 0;
     struct sockaddr_in addr;
     int sock;
-
     //get the server port from command line
     server_port = atoi(argv[1]);
 
- /*   while (fread(&curr_word, 1, , stdin) != 0) { // Read the data from input
-      // Do something with data stored in buffer
-    	if(UorP == 0){
-    		usernames[index] = curr_word;
-    		UorP = 1;
-			curr_word = "";
-    	}
-    	else{
-    		passwords[index] = curr_word;
-    		UorP = 0;
-    		index++;
-    	}
-    }*/
+    {
+    int c;
+    while ((c = getchar()) != EOF)
+    	putchar(tolower(c));
+    }
 
     printf("the server port is: %d\n",server_port);
 
